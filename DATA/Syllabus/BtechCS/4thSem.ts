bc047fe59e23638fa4b3a1290c6b8e19f0e36231
Nandalal Shukla <shuklanandalal@gmail.com>
2026-04-10 11:41:46 +0530

    added empty folders using git keep

---
+++ DATA/Syllabus/BtechCS/4thSem.ts
@@ -0,0 +1,37 @@
+//create syllabus data for 4th sem of btech cs in the same format as the 4th sem pyqs data
+
+//take file location from public folder and add it to the data rather than i manually adding the data for each subject in the pyqs data file.
+const SYLLABUS4_DATA_2024_25 = [
+  {
+    subject: "Database Management Systems",
+    code: "CS201",
+    credits: 3,
+    semester: 4,
+        year: "2024-25",
+        src:"../../../public/Syllabus/BtechCS/4thsem/DBMS Syllabus 4th Sem.pdf"
+  },
+  {
+    subject: "Theory of Computation",
+    code: "CSE 251",
+    credits: 4,
+    semester: 4,
+    year: "2024-25",
+    src:"../../../public/Syllabus/BtechCS/4thsem/TOC Syllabus 4th Sem.pdf"
+  },
+  {
+    subject: "Management for Engineers",
+    code: "HMM 305",
+    credits: 4,
+    semester: 4,
+    year: "2024-25",
+    src:"../../../public/Syllabus/BtechCS/4thsem/HMM 305 MANAGEMENT FOR ENGINEERS 2024-25.pdf"
+  },
+  {
+    subject: "Digital Forensics",
+    code: "CSC 201",
+    credits: 2,
+    semester: 4,
+    year: "2024-25",
+    src:"../../../public/PYQs/BtechCS/4thsem/CSC 201 DIGITAL FORENSICS 2024-25.pdf"
+  },
+];
