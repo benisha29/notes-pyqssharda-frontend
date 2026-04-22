21a0bed3e7bc3650a97f2232051f49a9a361d465
Nandalal Shukla <shuklanandalal@gmail.com>
2026-04-22 14:09:31 +0530

    started working on sharda social

---
+++ app/sharda-social/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ShardaSocial = () => {
+  return (
+    <div>
+      <div>The Social Networking Platform for Sharda University Students!</div>
+      <h1>The world is here, where are you?</h1>
+    </div>
+  );
+};
+
+export default ShardaSocial;
