95ebec7b93abe3be1d32b9f09d6542fe59584858
ben <benishasharma29@gmail.com>
2026-04-23 23:05:23 +0530

    added typescript code to 2ndSem and 4thSem in DATA

---
+++ DATA/Notes/BtechCS/4thSem.ts
@@ -0,0 +1,28 @@
+const NOTES4_DATA_2024_25 = [
+  {
+    subject: "Database Management Systems",
+    code: "CSE249",
+    credits: 4,
+    semester: 4,
+    year: "2024-25",
+    src: [
+      "/Notes/BtechCS/4thsem/Unit 3 CSE249 DBMS.pdf",
+      "/Notes/BtechCS/4thsem/Unit 4 CSE249 DBMS.pdf",
+      "/Notes/BtechCS/4thsem/Unit 5 CSE249 DBMS.pdf",
+    ],
+  },
+  {
+    subject: "Theory of Computation",
+    code: "CSE251",
+    credits: 4,
+    semester: 4,
+    year: "2024-25",
+    src: [
+      "/Notes/BtechCS/4thsem/Unit 3 CSE251 TOC.pdf",
+      "/Notes/BtechCS/4thsem/Unit 4 CSE251 TOC.pdf",
+      "/Notes/BtechCS/4thsem/Unit 5 CSE 251 TOC.pdf",
+    ],
+  },
+];
+
+export default NOTES4_DATA_2024_25;
